e5a0c445a0be7e8b7f428ae932721c5b5bd80039
MikolaSh <nikolashulga2000@gmail.com>
2021-11-06 17:56:23 +0300

    feat: change path.join to __dirname

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 let fs = require('fs');
 const path = require('path')
 
-let myText = fs.createReadStream(path.join('./01-read-file/text.txt'), 'utf-8');
+let myText = fs.createReadStream(path.join(__dirname + '/text.txt'), 'utf-8');
 
 myText.on('data', function(chunk){
     console.log(chunk);
