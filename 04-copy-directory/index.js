ff52b31d51b5654fe5039fbd08bc5f3dfdf9fa5c
MikolaSh <nikolashulga2000@gmail.com>
2021-11-08 14:25:00 +0300

    feat: 04-copy-directory complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+let fs = require('fs')
+const path = require('path')
 
+fs.mkdir(__dirname+'/files_copy', {recursive:true},(err)=>{
+    if(err){
+        console.log(err);
+        return
+    }
+})
+fs.readdir(__dirname+'/files',function(err,items){
+    items.forEach(function(item){
+        console.log(__dirname+'/'+item)
+        fs.copyFile(__dirname+'/files/'+item, __dirname+`/files_copy/${item}`,(err)=>{
+            if(err){
+                console.log(err);
+                return
+            }
+        } )
+    })
+})
