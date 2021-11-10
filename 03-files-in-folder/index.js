cd205c3ca470f1a2f764d13eccc7017e4c824cd6
MikolaSh <nikolashulga2000@gmail.com>
2021-11-10 11:34:48 +0300

    refactor: file name without extention, extension without dot

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,12 +3,12 @@ let fs = require('fs')
 const path = require('path')
 console.log(__dirname+'/secret-folder')
 
-fs.readdir(__dirname+'/secret-folder', function(err, items) {
-    console.log(items);
+fs.readdir(__dirname+'/secret-folder',{withFileTypes:true}, function(err, items) {
+
     items.forEach(function(item){
-        fs.stat(__dirname+'/secret-folder/'+item, function(err, stats){
-            if(stats.isFile()){
-                console.log(path.basename(item)+' - '+path.extname(item)+' - '+stats.size+'b') 
+        fs.stat(__dirname+'/secret-folder/'+item.name, function(err, stats){
+            if(item.isFile()){
+                console.log(path.parse(item.name).name+' - '+path.extname(item.name).slice(1)+' - '+stats.size+'b') 
             }
         })
     })
