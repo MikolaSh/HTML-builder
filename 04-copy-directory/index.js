8e7cb1653469728790fd22e4805d4da7d8adde46
MikolaSh <nikolashulga2000@gmail.com>
2021-11-11 15:15:49 +0300

    feat: in 04 task updated when deleted

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,21 +1,41 @@
 let fs = require('fs')
 const path = require('path')
 
-fs.mkdir(__dirname+'/files_copy', {recursive:true},(err)=>{
-    if(err){
-        console.log(err);
-        return
-    }
-})
-fs.readdir(__dirname+'/files',function(err,items){
-    items.forEach(function(item){
-        console.log(__dirname+'/'+item)
-        fs.copyFile(__dirname+'/files/'+item, __dirname+`/files_copy/${item}`,(err)=>{
-            if(err){
-                console.log(err);
-                return
-            }
-        } )
+
+async function makeDir(){
+    fs.mkdir(__dirname+'/files_copy',(err)=>{
+        if(err){
+            fs.readdir(__dirname+'/files_copy',function(err,items){
+                items.forEach(function(item){
+                    fs.rm(__dirname+'/files_copy/'+item,function(err){
+                        if(err){
+                            console.log(err)
+                            return
+                        }
+                    })
+                })
+            })
+        }
     })
-})
+
+}
+
+async function copyDir(){
+    await makeDir();
+    setTimeout(()=>{
+        fs.readdir(__dirname+'/files',function(err,items){
+            items.forEach(function(item){
+                fs.copyFile(__dirname+'/files/'+item, __dirname+`/files_copy/${item}`,(err)=>{
+                    if(err){
+                        console.log(err);
+                        return
+                    }
+                } )
+            })
+        })
+    },1000)
+
+    
+}
+copyDir()
 console.log('Самопроверку прошел на 150 баллов.\n Если найдете ошибку или неточно, пожалуйста, напишите мне в дискорд: mikola_the_human#9901')
